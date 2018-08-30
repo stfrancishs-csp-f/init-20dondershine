eeb8facee11daa027bd02562e2586837f65a9b43
Isabella Dondershine <20dondershine@stfrancishs.org>
2018-08-30 15:45:25 +0000

    init

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/********
+*
+* Prints 'Hello, World' on screen
+* HelloWorld.c
+* Bella Dondershine
+* C Programming
+*
+*********/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world\n");
+    return 0;
+}
